855bd9dc871302bd61360332dc79d140c9cb7688
Matias Bravo <mbravo@localhost.localdomain>
2021-07-15 20:25:14 +0800

    Started work on colourmap manipulation tools

--- scicm/__init__.py
+++ scicm/__init__.py
@@ -1,2 +1,6 @@
-# NEED TO PROPERLY SET UP THIS
+'''scicm is a small package containing several colour maps, aimed at filling gaps in the
+availability of good (i.e. perceptually linear) in both matplotlib and in other available
+packages (like cmocean and CMasher). All colour maps in this package have been created
+using viscm.'''
 from . import cm
+from . import tools
--- scicm/cm.py
+++ scicm/cm.py
@@ -1,32 +1,39 @@
-########################################
-# Set up the colour maps
-########################################
+'''This module reads the colour map data from the cm folder, set ups the corresponding colour
+maps and their corresponding reverse order maps, and registers them with matplotlib '''
 import os
 import numpy as np
 from matplotlib import cm
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap as LSC
 
+# List of all colour maps in scicm
 cmaps=['B2C','B2P','BkG','BkR','Blue','BwR','C2G',
        'CkO','Cyan','Day','Edges','G2Y','Garnet','GkP','Green',
        'GwP','M2R','Magenta','Night','O2Y','Orange',
        'P2M','PkO','Purple','Quartile','R2O','Red',
        'Ripe','Stone','Tropical','Yellow','YkM']
 
+# List of diverging colour maps, this separate list is used
+# to generate the mirror name copies (i.e. RwB)
 diverging={'BkG':'GkB','BkR':'RkB','BwR':'RwB','CkO':'OkC',
            'GkP':'PkG','GwP':'PwG','PkO':'OkP','YkM':'MkY'}
 
+# Reading in the colour map data and setting up the cm objects
 cmap_dict={}
 data_path=os.path.join(os.path.split(__file__)[0],'cm_data')
 for cmap in cmaps:
     cmap_path=os.path.join(data_path,f'{cmap}.txt')
     cmap_data=np.loadtxt(cmap_path)
-    cmap_dict[cmap]=LinearSegmentedColormap.from_list(cmap,cmap_data,N=cmap_data.shape[0])
-    cmap_dict[f'{cmap}_r']=LinearSegmentedColormap.from_list(f'{cmap}_r',cmap_data[::-1,:],N=cmap_data.shape[0])
+    cmap_dict[cmap]=LSC.from_list(cmap,cmap_data,N=cmap_data.shape[0])
+    cmap_dict[f'{cmap}_r']=LSC.from_list(f'{cmap}_r',cmap_data[::-1,:],
+                                         N=cmap_data.shape[0])
     if cmap in diverging.keys():
-        cmap_dict[f'{diverging[cmap]}_r']=LinearSegmentedColormap.from_list(cmap,cmap_data,N=cmap_data.shape[0])
-        cmap_dict[diverging[cmap]]=LinearSegmentedColormap.from_list(f'{cmap}_r',cmap_data[::-1,:],N=cmap_data.shape[0])
+        cmap_dict[f'{diverging[cmap]}_r']=LSC.from_list(cmap,cmap_data,
+                                                        N=cmap_data.shape[0])
+        cmap_dict[diverging[cmap]]=LSC.from_list(f'{cmap}_r',cmap_data[::-1,:],
+                                                 N=cmap_data.shape[0])
 
-for cmap in cmap_dict.keys():
+# Registering the maps with matplotlib
+for cmap in cmap_dict:
     cm.register_cmap(f'scicm.{cmap}',cmap_dict[cmap])
 
 locals().update(cmap_dict)
