b879fe27ac10dcc90c7e33f04a123102e623ba33
Matias Bravo <bravosam@mcmaster.ca>
2023-04-28 07:54:47 +0800

    Testing all possible errors for scicm.tools.crop

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -2,6 +2,17 @@ import scicm
 import pytest
 
 class Test_errors:
-    def test_crop_badvminvmax(self):
+    # Test if scicm.tools functions raise the appropiate errors
+    
+    # Test scicm.tools.crop raises error with vmin,vmax in the wrong order
+    def test_error_crop_0(self):
         with pytest.raises(ValueError):
             scicm.tools.crop(scicm.cm.Stone,0.6,0.4)
+    # Test scicm.tools.crop raises error with vmin,vmax outside of allowed values
+    def test_error_crop_0(self):
+        with pytest.raises(ValueError):
+            scicm.tools.crop(scicm.cm.Stone,0,2)
+    # Test scicm.tools.crop raises error if an undefined colour map is given
+    def test_error_crop_0(self):
+        with pytest.raises(TypeError):
+            scicm.tools.crop('gibberish',0.2,0.8)
