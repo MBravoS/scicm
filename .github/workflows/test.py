331c5cd4e0324d40cc34e745055d93f386e2fa7b
Matias Bravo <bravosam@mcmaster.ca>
2023-04-28 08:06:39 +0800

    Fixed and expanded scicm.tools.crop error tests

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -9,10 +9,18 @@ class Test_errors:
         with pytest.raises(ValueError):
             scicm.tools.crop(scicm.cm.Stone,0.6,0.4)
     # Test scicm.tools.crop raises error with vmin,vmax outside of allowed values
-    def test_error_crop_0(self):
+    def test_error_crop_1(self):
         with pytest.raises(ValueError):
             scicm.tools.crop(scicm.cm.Stone,0,2)
     # Test scicm.tools.crop raises error if an undefined colour map is given
-    def test_error_crop_0(self):
-        with pytest.raises(TypeError):
+    def test_error_crop_2(self):
+        with pytest.raises(ValueError):
             scicm.tools.crop('gibberish',0.2,0.8)
+    def test_error_crop_3(self):
+        with pytest.raises(ValueError):
+            scicm.tools.crop(3,0.2,0.8)
+    # Test scicm.tools.crop raises error if colour map name is not a string
+    def test_error_crop_2(self):
+        with pytest.raises(TypeError):
+            scicm.tools.crop(0,0.2,0.8,8)
+
