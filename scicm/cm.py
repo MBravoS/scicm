df8ed0e645af8476cb8f3548d909c116a1a64495
Matias Bravo <bravosam@mcmaster.ca>
2024-06-14 07:01:46 +0800

    Added examples to the documentation of the three tool functions.

--- scicm/cm.py
+++ scicm/cm.py
@@ -1,55 +1,61 @@
 '''This module reads the colour map data from the cm folder, set ups the corresponding colour
 maps and their corresponding reverse order maps, and registers them with matplotlib'''
 
-from os import path, sep
-import pkg_resources
-from glob import glob
-from numpy import loadtxt
-from matplotlib.colors import LinearSegmentedColormap as LSC
+
+from os import path as _path, sep as _sep
+import pkg_resources as _pkg_resources
+from glob import glob as _glob
+from numpy import loadtxt as _loadtxt
+from matplotlib.colors import LinearSegmentedColormap as _LSC
+
 
 # Check for matplotlib version
-mpl_ver = pkg_resources.get_distribution('matplotlib').version
-mpl_ver = [int(s) for s in mpl_ver.split('.')]
-mpl_ver = mpl_ver[0]*10+mpl_ver[1]
-if mpl_ver >= 35:
-    from matplotlib import colormaps as cm
+_mpl_ver = _pkg_resources.get_distribution('matplotlib').version
+_mpl_ver = [int(s) for s in _mpl_ver.split('.')]
+_mpl_ver = _mpl_ver[0]*10+_mpl_ver[1]
+if _mpl_ver >= 35:
+    from matplotlib import colormaps as _cm
 else:
-    from matplotlib import cm
+    from matplotlib import cm as _cm
+
 
 # Grab the path to the colour map data
-data_path = path.join(path.split(__file__)[0], 'cm_data')
+_data_path = _path.join(_path.split(__file__)[0], 'cm_data')
+
 
 # List of all colour maps in scicm
+_cmaps = [c.split(_sep)[-1].split('.')[0] for c in sorted(_glob(f'{_data_path}{_sep}*.txt'))]
 
-cmaps = [c.split(sep)[-1].split('.')[0] for c in sorted(glob(f'{data_path}{sep}*.txt'))]
 
 # List of diverging colour maps, this separate list is used
 # to generate the mirror name copies (i.e. RwB)
-diverging = [c for c in cmaps if len(c) == 3 and c[1] == 'k']
-diverging += [c for c in cmaps if len(c) == 3 and c[1] == 'w']
-diverging = {c: c[::-1] for c in diverging}
+_diverging = [c for c in _cmaps if len(c) == 3 and c[1] == 'k']
+_diverging += [c for c in _cmaps if len(c) == 3 and c[1] == 'w']
+_diverging = {c: c[::-1] for c in _diverging}
+
 
 # Reading in the colour map data and setting up the cm objects
-cmap_dict = {}
-for cmap in cmaps:
-    cmap_path = path.join(data_path, f'{cmap}.txt')
-    cmap_data = loadtxt(cmap_path)
-    cmap_dict[cmap] = LSC.from_list(f'scicm.{cmap}', cmap_data, N=cmap_data.shape[0])
-    cmap_dict[f'{cmap}_r'] = LSC.from_list(f'scicm.{cmap}_r', cmap_data[::-1, :],
-                                           N=cmap_data.shape[0])
-    if cmap in diverging.keys():
-        cmap_name = diverging[cmap]
-        cmap_dict[cmap_name] = LSC.from_list(f'scicm.{cmap_name}', cmap_data[::-1, :],
-                                             N=cmap_data.shape[0])
-        cmap_dict[f'{cmap_name}_r'] = LSC.from_list(f'scicm.{cmap_name}_r', cmap_data,
-                                                    N=cmap_data.shape[0])
+_cmap_dict = {}
+for _cmap in _cmaps:
+    _cmap_path = _path.join(_data_path, f'{_cmap}.txt')
+    _cmap_data = _loadtxt(_cmap_path)
+    _cmap_dict[_cmap] = _LSC.from_list(f'scicm.{_cmap}', _cmap_data, N=_cmap_data.shape[0])
+    _cmap_dict[f'{_cmap}_r'] = _LSC.from_list(f'scicm.{_cmap}_r', _cmap_data[::-1, :],
+                                           N=_cmap_data.shape[0])
+    if _cmap in _diverging.keys():
+        _cmap_name = _diverging[_cmap]
+        _cmap_dict[_cmap_name] = _LSC.from_list(f'scicm.{_cmap_name}', _cmap_data[::-1, :],
+                                             N=_cmap_data.shape[0])
+        _cmap_dict[f'{_cmap_name}_r'] = _LSC.from_list(f'scicm.{_cmap_name}_r', _cmap_data,
+                                                    N=_cmap_data.shape[0])
+
 
 # Registering the maps with matplotlib
-if mpl_ver >= 35:
-    for cmap in cmap_dict:
-        cm.register(cmap_dict[cmap])
+if _mpl_ver >= 35:
+    for _cmap in _cmap_dict:
+        _cm.register(_cmap_dict[_cmap])
 else:
-    for cmap in cmap_dict:
-        cm.register_cmap(f'scicm.{cmap}', cmap_dict[cmap])
+    for _cmap in _cmap_dict:
+        _cm.register_cmap(f'scicm.{_cmap}', _cmap_dict[_cmap])
 
-locals().update(cmap_dict)
+locals().update(_cmap_dict)
--- scicm/tools.py
+++ scicm/tools.py
@@ -1,13 +1,18 @@
 '''This module contain auxiliary functions for easy manipulation of the scicm colour maps.'''
 
 
-import numpy as np
-import matplotlib as mpl
-from matplotlib.colors import LinearSegmentedColormap as LSC
+import numpy as _np
+import matplotlib as _mpl
+from matplotlib.colors import LinearSegmentedColormap as _LSC
 
+### TODO: Need to figure out with merge is not producing a perfect match to the input colour maps
 
 def crop(cmapin, vmin=0.0, vmax=1.0, name_newcmap=None):
-    '''Crop colour map between vmin/vmax values. Can also register the new colour map with matplotlib.
+    '''Takes a colour map and from a subset of colours generates a new map.
+    
+    From the input colour map, selects all colours that lie between the given vmin/vmax values and
+    returns a new colour map that includes only the selected subset of colours. Can also register
+    the new colour map with matplotlib if a name for the new colour map is given.
     
     Parameters
     ----------
@@ -19,10 +24,33 @@ def crop(cmapin, vmin=0.0, vmax=1.0, name_newcmap=None):
         input colourmap).
     name_newcmap : str, optional
         If given, defines the name to register with matplotlib.
+    
     Returns
     -------
     newcmap
         Colourmap object with the new colour map.
+    
+    Example
+    -------
+    Generate a new colour map that only spans the brighter half of matplolib's viridis:
+    
+    >>> from matplotlib.cm import viridis
+    >>> from scicm.tools import crop
+    
+    >>> short_viridis = crop(viridis, vmin=0.5)
+    
+        Check that the first colour in the new colour map corresponds to the mid-point of viridis.
+    >>> viridis(128)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> short_viridis(0)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    
+        Check that the last colour in the new colour map corresponds to the last colour of viridis.
+    >>> viridis(255)
+    (0.993248, 0.906157, 0.143936, 1.0)
+    >>> short_viridis(255)
+    (0.993248, 0.906157, 0.143936, 1.0)
+    
     '''
     
     if vmin > vmax:
@@ -33,7 +61,7 @@ def crop(cmapin, vmin=0.0, vmax=1.0, name_newcmap=None):
         raise TypeError('name_newcmap must be a string')
     
     if isinstance(cmapin, str):
-        cmapin = mpl.colormaps[cmapin]
+        cmapin = _mpl.colormaps[cmapin]
     try:
         test = cmapin(0.5)
     except TypeError:
@@ -44,20 +72,26 @@ def crop(cmapin, vmin=0.0, vmax=1.0, name_newcmap=None):
     except TypeError:
         raise TypeError('cmapin does not behave like a colour map')
     
-    cmap_data = cmapin(np.linspace(vmin, vmax, 256))
+    cmap_data = cmapin(_np.linspace(vmin, vmax, 256))
     
     if name_newcmap:
-        newcmap = LSC.from_list(name_newcmap, cmap_data, N=cmap_data.shape[0])
-        mpl.colormaps.register(name_newcmap, newcmap)
+        newcmap = _LSC.from_list(name_newcmap, cmap_data, N=cmap_data.shape[0])
+        _mpl.colormaps.register(name_newcmap, newcmap)
     else:
-        newcmap = LSC.from_list('newcmap', cmap_data, N=cmap_data.shape[0])
+        newcmap = _LSC.from_list('newcmap', cmap_data, N=cmap_data.shape[0])
     
     return newcmap
 
 
 def stitch(cmapinlist, vlims, tpoints, name_newcmap=None):
-    '''Stich together the selected crops from the given list of colour maps. Can also register the
-    new colour map with matplotlib.
+    '''Takes a list of colour maps and generates a new one that includes stretched/compressed
+    segments from each.
+    
+    From the input list of colour maps, selects all colours that lie between the given transition
+    points and returns a new colour map that includes only the selected subsets of colours. The
+    colour subsets drawn from each colour map on the list are defined by the list of value limits.
+    Can also register the new colour map with matplotlib if a name for the new colour map is given.
+    
     
     Parameters
     ----------
@@ -73,72 +107,111 @@ def stitch(cmapinlist, vlims, tpoints, name_newcmap=None):
         colour map to the next.
     name_newcmap : str, optional
         If given, defines the name to register with matplotlib.
+    
     Returns
     -------
     newcmap
         Colourmap object with the new colour map.
+    
+    Example
+    -------
+    Generate a new colour map where the first half are viridis colours and the second half are magma
+    colours:
+    
+    >>> from matplotlib.cm import viridis, magma
+    >>> from scicm.tools import stitch
+    
+    >>> viridis_magma = stitch([viridis,magma], tpoints=[0.5])
+    
+        Check that the first colour in the new colour map corresponds to the first of viridis.
+    >>> viridis(0)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> viridis_magma(0)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    
+        Check that the the new colour map switched from viridis to magma at the mid-point.
+    >>> viridis(127)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> viridis_magma(127)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> magma(128)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> viridis_magma(128)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    
+        Check that the last colour in the new colour map corresponds to the last colour of magma.
+    >>> magma(255)
+    (0.993248, 0.906157, 0.143936, 1.0)
+    >>> viridis_magma(255)
+    (0.993248, 0.906157, 0.143936, 1.0)
+    
     '''
     
     if not isinstance(cmapinlist, list):
         raise TypeError('cmapinlist must be a list')
     if len(cmapinlist)<2:
         raise ValueError('cmapinlist must contain at least two colour maps')
-    if not isinstance(vlims, np.ndarray):
-        vlims = np.array(vlims)
+    if not isinstance(vlims, _np.ndarray):
+        vlims = _np.array(vlims)
     if vlims.shape!=(len(cmapinlist),2):
         raise ValueError('vlims shape does not match expectation of (N,2)')
     for vcheck in vlims:
         if vcheck[0] > vcheck[1]:
             raise ValueError('The value of vmin must be lower than vmax')
-    if np.sum((vlims < 0.0) | (vlims > 1.0)) > 0:
+    if _np.sum((vlims < 0.0) | (vlims > 1.0)) > 0:
         raise ValueError('The values in vlims must be in the closed range [0, 1]')
-    if not isinstance(tpoints, np.ndarray):
-        tpoints = np.array(tpoints)
-    if np.sum(np.diff(tpoints) < 0) > 0:
+    if not isinstance(tpoints, _np.ndarray):
+        tpoints = _np.array(tpoints)
+    if _np.sum(_np.diff(tpoints) < 0) > 0:
         raise ValueError('tpoints must be monotonically increasing in value')
-    if np.sum((tpoints <= 0.0) | (tpoints >= 1.0)):
+    if _np.sum((tpoints <= 0.0) | (tpoints >= 1.0)):
         raise ValueError('The values of tpoints must be in the open range (0, 1)')
     if not isinstance(name_newcmap, str) and name_newcmap is not None:
         raise TypeError('name_newcmap must be a string')
     
-    cmapinlist = [mpl.colormaps[cmapin] if isinstance(cmapin, str) else cmapin for cmapin in cmapinlist]
+    cmapinlist = [_mpl.colormaps[cmapin] if isinstance(cmapin, str) else cmapin for cmapin in cmapinlist]
     try:
         test = [c(0.5) for c in cmapinlist]
     except TypeError:
         raise TypeError('One of the elements in cmapinlist does not behave like a colour map')
     try:
-        if np.sum([len(t)!=4 for t in test])<len(test):
+        if _np.sum([len(t)==4 for t in test])<len(test):
             raise TypeError('One of the elements in cmapinlist does not behave like a colour map')
     except TypeError:
         raise TypeError('One of the elements in cmapinlist does not behave like a colour map')
     
-    tpoints = np.array([0]+list(tpoints)+[1])
-    nstep = np.empty(len(cmapinlist))
-    test_range = np.linspace(0, 1, 256)
+    tpoints = _np.array([0]+list(tpoints)+[1])
+    nstep = _np.empty(len(cmapinlist))
+    test_range = _np.linspace(0, 1, 256)
     for i in range(len(cmapinlist)):
-        nstep[i] = np.sum((test_range > tpoints[i]) & (test_range < tpoints[i+1])) + 1
-    nstep = np.where(nstep < 1, 1, nstep)
-    if np.sum(nstep) != 256:
-        nstep[-1] += 256-np.sum(nstep)
+        nstep[i] = _np.sum((test_range > tpoints[i]) & (test_range < tpoints[i+1])) + 1
+    nstep = _np.where(nstep < 1, 1, nstep)
+    if _np.sum(nstep) != 256:
+        nstep[-1] += 256-_np.sum(nstep)
     nstep = nstep.astype('int')
     
-    cmaplist_data = [cmapinlist[i](np.linspace(vlims[i, 0], vlims[i, 1], nstep[i]))
+    cmaplist_data = [cmapinlist[i](_np.linspace(vlims[i, 0], vlims[i, 1], nstep[i]))
                      for i in range(len(cmapinlist))]
-    cmap_data = np.concatenate(cmaplist_data, axis=0)
+    cmap_data = _np.concatenate(cmaplist_data, axis=0)
     
     if name_newcmap:
-        newcmap = LSC.from_list(name_newcmap, cmap_data, N=cmap_data.shape[0])
-        mpl.colormaps.register(name_newcmap, newcmap)
+        newcmap = _LSC.from_list(name_newcmap, cmap_data, N=cmap_data.shape[0])
+        _mpl.colormaps.register(name_newcmap, newcmap)
     else:
-        newcmap = LSC.from_list('newcmap', cmap_data, N=cmap_data.shape[0])
+        newcmap = _LSC.from_list('newcmap', cmap_data, N=cmap_data.shape[0])
     
     return newcmap
 
 
 def merge(cmapinlist, tpoints, name_newcmap=None):
-    '''Merge together the selected crops from the given list of colour maps. Can also register the
-    new colour map with matplotlib. This is a light wrapper around scicm.tools.stitch, which uses
-    the transition points as the vmin/vmax values to crop each input colour map.
+    '''Takes a list of colour maps and generates a new one that includes segments from each.
+    
+    From the input list of colour maps, selects all colours that lie between the given transition
+    points and returns a new colour map that includes only the selected subsets of colours. The
+    colour subset drawn from the first colour map on the list goes from the first colour up to the
+    first transition point, and the subset drawn from the last colour map goes from the last
+    transition point up to the last colour in the map. Can also register the new colour map with
+    matplotlib if a name for the new colour map is given.
     
     Parameters
     ----------
@@ -150,25 +223,59 @@ def merge(cmapinlist, tpoints, name_newcmap=None):
         colour map to the next.
     name_newcmap : str, optional
         If given, defines the name to register with matplotlib.
+    
     Returns
     -------
     newcmap
         Colourmap object with the new colour map.
+    
+    Example
+    -------
+    Generate a new colour map where the first half are viridis colours and the second half are magma
+    colours:
+    
+    >>> from matplotlib.cm import viridis, magma
+    >>> from scicm.tools import merge
+    
+    >>> viridis_magma = merge([viridis,magma], tpoints=[0.5])
+    
+        Check that the first colour in the new colour map corresponds to the first of viridis.
+    >>> viridis(0)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> viridis_magma(0)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    
+        Check that the the new colour map switched from viridis to magma at the mid-point.
+    >>> viridis(127)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> viridis_magma(127)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> magma(128)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    >>> viridis_magma(128)
+    (0.127568, 0.566949, 0.550556, 1.0)
+    
+        Check that the last colour in the new colour map corresponds to the last colour of magma.
+    >>> magma(255)
+    (0.993248, 0.906157, 0.143936, 1.0)
+    >>> viridis_magma(255)
+    (0.993248, 0.906157, 0.143936, 1.0)
+    
     '''
     
     if not isinstance(cmapinlist, list):
         raise TypeError('cmapinlist must be a list')
-    if not isinstance(tpoints, np.ndarray):
-        tpoints = np.array(tpoints)
-    if np.sum(np.diff(tpoints) < 0) > 1:
+    if not isinstance(tpoints, _np.ndarray):
+        tpoints = _np.array(tpoints)
+    if _np.sum(_np.diff(tpoints) < 0) > 1:
         raise ValueError('tpoints must be monotonically increasing in value')
-    if np.sum((tpoints <= 0.0) | (tpoints >= 1.0)):
+    if _np.sum((tpoints <= 0.0) | (tpoints >= 1.0)):
         raise ValueError('The values of tpoints must be in the open range (0, 1)')
     if not isinstance(name_newcmap, str) and name_newcmap is not None:
         raise TypeError('name_newcmap must be a string')
     
-    vlims = np.array([0]+list(tpoints)+[1])
-    vlims = np.array([[vlims[i], vlims[i+1]] for i in range(len(cmapinlist))])
+    vlims = _np.array([0]+list(tpoints)+[1])
+    vlims = _np.array([[vlims[i], vlims[i+1]] for i in range(len(cmapinlist))])
     newcmap = stitch(cmapinlist, vlims, tpoints, name_newcmap)
     
     for vcheck in vlims:
